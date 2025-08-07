79a3253b28f7486f5ec32fcd7e1c04995d7d0eef
Ivanna Platthy <npdpdev@gmail.com>
2025-08-07 03:00:03 -0400

    Implemented rudamentary error-checking on sending Returned data no longer includes ID, but should make use of it in the future to ensure data is received in order.

--- braviawrap/braviaREST.ts
+++ braviawrap/braviaREST.ts
@@ -18,16 +18,32 @@ export class braviaREST {
 
 
 
-    //send data and return the message          IMPLEMENT TRY:CATCH     TRY WITH AND WITHOUT DATA
+    //send data and return the message          IMPLEMENT SYSTEM USING "ID" FIELD TO ENSURE CORRECT ORDER OF RESPONSES RECEIVED. (Bravia's JSON-RPC protocol reserves value 0)
     public async send(jsonData?:string){
-        const response = await fetch(this.fullURL, {
-        method: 'POST',
-        body: jsonData,
-        headers: this.packagedHeader
-    });
 
-    const data = await response.json();
-    return data;
+        try{
+
+            const response = await fetch(this.fullURL, {
+            method: 'POST',
+            body: jsonData,
+            headers: this.packagedHeader
+        });
+
+        //if HTTP response ! in range 200-299, throw an error
+        if (!response.ok){
+            const message = "Error: "+response.status;
+            throw new Error(message);
+        }
+
+        //returns "result" field of response
+        const data = await response.json();
+        return data.result;
+
+
+
+        }catch(error){
+            console.error(error);
+        }
 
 
     }
